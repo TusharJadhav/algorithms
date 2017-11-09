59c301aec96416bdf32e076e53faa76e18ea530e
Tushar Jadhav <tushar.s.jadhav@gmail.com>
2017-11-09 05:55:52 -0800

    Adding stack implementation using resizing array along with the tests

---
+++ algorithms/include/algorithms/stack_using_resizing_array.h
@@ -0,0 +1,82 @@
+#ifndef INCLUDE_ALGORITHMS_STACK_USING_RESIZING_ARRAY_H_
+#define INCLUDE_ALGORITHMS_STACK_USING_RESIZING_ARRAY_H_
+#include "stack_interface.h"
+#include <memory>
+#include <utility>
+#include <stdexcept>
+
+/*
+This algorithm "Quick Union With Path Compression" solves the dynamic connectivity problem.
+Starting from an empty data structure, any sequence of M union-find ops on N objects makes ≤ c ( N + M lg* N ) array accesses.
+In reality log * function can be considered to be at the most 5.
+Thus in theory, this algorithm is not quite linear but in practice it is.
+*/
+
+namespace algorithms {
+  template<typename _tp>
+  class StackUsingResizingArray : public StackInterface<_tp> {
+  public:
+    typedef _tp value_type;
+
+    // StackInterface methods
+    void Push(const value_type& val) {
+      if(size_ == capacity_)
+        resize(capacity_ * 2);
+
+      data_[size_++] = val;
+    }
+
+    void Pop() {
+      if(Empty())
+        throw std::underflow_error("Trying to pop element from an empty stack");
+
+      --size_;
+      // Todo: How do we make sure that the destructor of the element being popped is called? 
+      if(size_ == capacity_ / 4)
+        resize(capacity_ / 2);
+    }
+
+    value_type& Top() {
+      if(Empty())
+        throw std::underflow_error("Trying to top element from an empty stack");
+
+      return data_[size_ - 1];
+    }
+
+    const value_type& Top() const {
+      if(Empty())
+        throw std::underflow_error("Trying to top element from an empty stack");
+
+      return data_[size_ - 1];
+    }
+
+    size_t Size() const {
+        return size_;
+    }
+
+    bool Empty() const {
+        return size_ == 0;
+    }
+
+  private:
+    void resize(size_t size) {
+      auto new_data = std::make_unique<value_type[]>(size);
+      
+      for(size_t index = 0; index < size_; ++index) {
+        new_data[index] = data_[index];
+      }
+
+      data_.reset(nullptr);
+      data_ = std::move(new_data);
+      capacity_ = size;
+    }
+
+  private:
+    const size_t initial_size_{2};
+    std::unique_ptr<value_type[]> data_{std::make_unique<value_type[]>(initial_size_)};
+    size_t capacity_{initial_size_};
+    size_t size_{0};
+  };
+}
+
+#endif //INCLUDE_ALGORITHMS_STACK_USING_LINK_LIST_H_ 
---
+++ algorithms/tests/stack_using_resizing_array_tests.cc
@@ -0,0 +1,93 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "stack_using_resizing_array.h"
+#include <string>
+
+using namespace testing;
+using namespace algorithms;
+
+TEST(StackUsingLinkListTests, TestInitiallEmpty) {
+    StackUsingResizingArray<int> my_stack;
+    EXPECT_TRUE(my_stack.Empty()) << "Initially the stack is supposed to be empty";
+}
+
+TEST(StackUsingLinkListTests, TestInitialSizeZero) {
+    StackUsingResizingArray<int> my_stack;
+    EXPECT_EQ(my_stack.Size(), 0) << "Initially the stack size should be zero since there are no elements pushed onto it";
+}
+
+TEST(StackUsingLinkListTests, TestPushPopAndTopOperations) {
+    StackUsingResizingArray<int> my_stack;
+    my_stack.Push(10);
+    my_stack.Push(20);
+    my_stack.Push(30);
+
+    ASSERT_EQ(my_stack.Top(), 30) << "Top element is not correct";
+
+    my_stack.Pop();
+
+    ASSERT_EQ(my_stack.Top(), 20) << "Top element is not correct";
+
+    my_stack.Push(40);
+
+    ASSERT_EQ(my_stack.Top(), 40) << "Top element is not correct";
+
+    my_stack.Pop();
+    my_stack.Pop();
+   
+    ASSERT_EQ(my_stack.Top(), 10) << "Top element is not correct";
+}
+
+TEST(StackUsingLinkListTests, TestNonEmpty) {
+    StackUsingResizingArray<int> my_stack;
+    my_stack.Push(10);
+    my_stack.Push(20);
+    my_stack.Push(30);
+
+    EXPECT_FALSE(my_stack.Empty()) << "Stack is empty after pushing few elements";
+}
+
+TEST(StackUsingLinkListTests, TestEmpty) {
+  StackUsingResizingArray<std::string> my_stack;
+  my_stack.Push("Hi");
+  my_stack.Push("Hello");
+
+  my_stack.Pop();
+  my_stack.Pop();
+
+  EXPECT_TRUE(my_stack.Empty()) << "Stack is not empty after popping all elements";
+}
+
+TEST(StackUsingLinkListTests, TestSizeAfterPushPopOperations) {
+  StackUsingResizingArray<int> my_stack;
+  
+  my_stack.Push(100);
+  my_stack.Push(200);
+  my_stack.Push(300);
+  my_stack.Push(400);
+
+  ASSERT_EQ(my_stack.Size(), 4) << "Stack size is incorrect";
+
+  my_stack.Pop();
+
+  ASSERT_EQ(my_stack.Size(), 3) << "Stack size is incorrect";
+
+  my_stack.Pop();
+  my_stack.Pop();
+
+  ASSERT_EQ(my_stack.Size(), 1) << "Stack size is incorrect";
+
+  my_stack.Pop();
+
+  ASSERT_EQ(my_stack.Size(), 0) << "Stack size is incorrect";
+}
+
+TEST(StackUsingLinkListTests, TestPoppingFromEmptyStack) {
+  StackUsingResizingArray<int> my_stack;
+  ASSERT_THROW(my_stack.Pop(), std::underflow_error) << "Popping from an stack  did not throw an exception";
+}
+
+TEST(StackUsingLinkListTests, TestAccessingFrontElementFromEmptyList) {
+  StackUsingResizingArray<double> my_stack;
+  ASSERT_THROW(my_stack.Top(), std::underflow_error) << "Accessing top element from an empty stack did not throw an exception"; 
+}
